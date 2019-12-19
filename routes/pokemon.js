b70f239269bc3f8bc4615b3423e6a64060fd0b92
pnalvarez <pedronalvarez@hotmail.com>
2019-12-18 23:56:32 -0300

    deleting pokemon from mongodb

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -16,17 +16,18 @@ const pokemonSchema = new mongoose.Schema({
                                           })
 const Pokemon = mongoose.model('Pokemon', pokemonSchema)
 
-router.get('', (req,res)=>{
-    res.render('index',{title: 'My Express App', message: 'Message'})
+router.get('', async (req,res)=>{
+    const pokemon = await Pokemon.find()
+    
+    if(!pokemon){
+        res.status(404).send('Pokemon not found')
+        return
+    }
+    res.status(200).send(pokemon)
 })
 
 router.get('/:id', async (req,res)=>{
-    // const pokemon = pokemons.find(pokemon => pokemon.id == parseInt(req.params.id))
-    
-    // if(!pokemon){
-    //     res.status(404).send('pokemon not found')
-    // }
-    // res.send(pokemon.name)
+   
     const pokemon = await Pokemon.find({_id: req.params.id})
     
     if(pokemon){
@@ -40,39 +41,36 @@ router.post('',async (req,res)=>{
     const pokemon = {
         name: req.body.name
     }
-    const exists = await pokemonExists(pokemon.name)
 
-    if (exists){
-        res.status(400).send('Pokemon already exists')
-        return
-    }
     const result = await createPokemon(pokemon.name)
     res.status(200).send(result)
 })
 
-router.put('/:id',(req,res)=>{
-    const pokemon = pokemons.find(p=>p.id === parseInt(req.params.id))
+router.put('/:id',async (req,res)=>{
 
-    if(!pokemon){
-       return res.status(400).send('Pokemon does not exist')
+    const id = req.params.id
+    const name = req.body.name
+    const exists = await pokemonExists(id)
+
+    if(!exists){
+        res.status(400).send('Pokemon does not exist')
+        return
     }
-    pokemon.name = req.body.name
-
-    res.status(200).send(pokemon)
+    const result = await updatePokemon(id, name)
+    res.status(200).send(result)
 })
 
-router.delete('/:id',(req,res)=>{
-    const pokemon = pokemons.find(p=>p.id === parseInt(req.params.id))
+router.delete('/:id',async (req,res)=>{
+    const id = req.params.id
+    console.log(id)
+    const exists = await pokemonExists(id)
 
-    if(!pokemon){
-       return res.status(400).send('Pokemon does not exist')
+    if(!exists){
+        res.status(404).send('Pokemon not found')
+        return
     }
-
-    const {id} = pokemon.id
-    const index = parseInt(id)
-
-    pokemons.splice(index,1)
-    res.send(pokemon)
+    const result = await deletePokemon(id)
+    res.status(200).send(result)
 })
 
 const createPokemon = async (name) => {
@@ -90,8 +88,8 @@ const createPokemon = async (name) => {
     }
 }
 
-const pokemonExists =  async (name) => {
-    const count = await Pokemon.find({name: name}).count()
+const pokemonExists =  async (id) => {
+    const count = await Pokemon.find({_id: id}).count()
     console.log(count)
     if(count > 0){
         console.log('true')
@@ -99,6 +97,16 @@ const pokemonExists =  async (name) => {
     } 
     console.log('false')
     return false
+}
+
+const updatePokemon = async (id, name) => {
+    const pokemon = await Pokemon.findByIdAndUpdate(id, {$set: {name: name}})
+    return pokemon
+}
+
+const deletePokemon = async (id) => {
+    const result = await Pokemon.deleteOne({_id: id})
+    return result
 }
 
 module.exports = router
